c76667168d6333220b10d895b2f5ac4cf19dab64
pan93412 <pan93412@gmail.com>
2022-12-28 19:55:43 +0800

    init: Laravel Breeze

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Contact;
+use App\Models\Pricing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,13 +15,21 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Placeholder
+        User::factory()
+            ->count(24)
+            ->has(Contact::factory()->count(4))
+            ->has(
+                Book::factory()->count(6)
+                    ->has(Pricing::factory()->count(3))
+            )
+            ->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'SharbookAdmin',
+            'email' => 'admin@sharbook.io',
+        ]);
     }
 }
